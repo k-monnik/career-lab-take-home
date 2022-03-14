399680e51995e3a63c900e459f24c43d5e5be477
Kristen Monnik <nolaviolinist@gmail.com>
2022-03-13 20:44:37 -0400

    add text conversion functions

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [textInput, setTextInput] = React.useState('Here is some example text.');
-  const [conversionMode, setConversionMode] = React.useState('lowercase');
-  const [textOutput, setTextOutput] = React.useState('');
+  const [textInput, setTextInput] = useState('');
+  const [conversionMode, setConversionMode] = useState('lowercase');
+  const [textOutput, setTextOutput] = useState('');
 
-  const handleRadioChange = event => {
-    setConversionMode(event.target.value);
+  const handleRadioChange = (e) => {
+    setConversionMode(e.target.value);
   }
 
-  const handleTextareaChange = event => {
-    setTextInput(event.target.value);
+  const handleTextareaChange = (e) => {
+    setTextInput(e.target.value);
   };
+
+  let text = textInput;
+
+  const result = () => {
+    if (conversionMode === "lowercase") {
+      let textLower = text.toLowerCase();
+      return textLower;
+    } else {
+      if (conversionMode === "uppercase") {
+        let textUpper = text.toUpperCase();
+        return textUpper;
+      }
+    }
+  }
 
   const handleSubmit = event => {
     event.preventDefault();
-    setTextOutput('Your formatted text will go here!')
+    setTextOutput(result);
+
   };
 
   return (
@@ -55,10 +71,11 @@ function App() {
           />
           <label htmlFor="conversion-1">Convert text to uppercase</label>
         </div>
-        <button type="button">Submit</button>
+        <button className="button" type="submit">Submit</button>
         <div className="form-control form-control__text u-mt-3">
           <label htmlFor="result">Converted text:</label>
-          <output id="result" class="result">{textOutput}</output>
+          <output id="result" className="result">{textOutput}</output>
+
         </div>
       </form>
     </div>
